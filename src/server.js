67f3233ad3690b0f55867d490cb008e23e6e7b48
heisen-bug <vatsalckc@gmail.com>
2019-12-29 18:32:02 +0530

    Basic Node app created

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+require('./db/mongoose')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+
+
+app.listen(port, () => {
+    console.log(`server is running on ${port}`)
+})
